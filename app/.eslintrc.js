5e5f3bbdcd9292f7a56941ea5b6345197f090fe1
alberto hortelano <alberto.hortelano.moran@gmail.com>
2020-06-09 11:21:19 +0200

    add redux toolkit and react router

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    'jest/globals': true,
-  },
-  extends: ['plugin:react/recommended', 'prettier', 'eslint:recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier', '@typescript-eslint', 'jest'],
-  rules: {
-    'prettier/prettier': ['error'],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+		'jest/globals': true,
+	},
+	extends: ['plugin:react/recommended', 'prettier', 'eslint:recommended'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier', '@typescript-eslint', 'jest'],
+	rules: {
+		'prettier/prettier': ['error'],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 };
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './state/reducers';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const store = configureStore({ reducer });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<Route path="/" render={() => <App />} />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
-console.log('NONONON  NONONONON  dsfasadfsadfsfsdf sdaf sadf sdf sad f');
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
